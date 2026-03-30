de8e6dc1115db6fc3e9caa6717dd8dd89c42fed6
hungerbk <wjantlr@gmail.com>
2026-03-31 02:57:24 +0900

    chore: tailwind 설정
    - 테마, cn 유틸함수, global.css 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,64 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
+    './src/features/**/*.{ts,tsx}',
+    './src/issues/**/*.{ts,tsx}',
+    './src/projects/**/*.{ts,tsx}',
+    './src/topics/**/*.{ts,tsx}',
+    './src/mypage/**/*.{ts,tsx}',
+    './src/invite/**/*.{ts,tsx}',
+    './src/providers/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wordcloud: {
+          100: '#98c9a3',
+          200: '#8cb369',
+          300: '#248277',
+          400: '#55a630',
+          500: '#4c956c',
+          600: '#2c6e49',
+        },
+      },
+      borderRadius: {
+        half: '50%',
+        large: '16px',
+        medium: '12px',
+        small: '8px',
+      },
+      fontSize: {
+        xl: '28px',
+        xxl: '22.5px',
+        large: '18px',
+        medium: '14px',
+        small: '12px',
+        xs: '10px',
+      },
+      fontWeight: {
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+      },
+      zIndex: {
+        hide: '-1',
+        base: '0',
+        important: '9999',
+        selected: '100',
+        sticky: '200',
+        backdrop: '300',
+        modal: '400',
+        popover: '500',
+        overlay: '600',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
